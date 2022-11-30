10f9a4c116e3d880876131f3ec9234cd9d4a0b63
LauricioX <developerwebmasterpro@gmail.com>
2022-11-30 20:57:14 -0300

    validação pt2

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,8 +2,21 @@ class UserController {
   async index(req, res) {}
 
   async create(req, res) {
-    console.log(req.body);
-    res.send("tesde de controlador de rotas");
+    let { email, name, password } = req.body;
+    if (email == undefined) {
+      res.status(403);
+      res.json({ erro: "o email é inválido!" });
+    }
+    if (name == undefined) {
+      res.status(403);
+      res.json({ erro: "o nome é inválido!" });
+    }
+    if (password == undefined) {
+      res.status(403);
+      res.json({ erro: "o password  é inválido!" });
+    }
+
+    res.status(200);
   }
 }
 
--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use("/", router);
 
-app.listen(3000, () => {
+app.listen(3636, () => {
   console.log("Servidor rodando");
 });
